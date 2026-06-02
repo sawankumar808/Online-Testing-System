4df981403d44f1dd52289ff886e2c47bc5588ec6
sawan kohli <kohlisawan808@gmail.com>
2026-06-03 01:56:25 +0530

    six static file

--- OTSproject/OTSproject/settings.py
+++ OTSproject/OTSproject/settings.py
@@ -69,6 +69,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'OTS' / 'static']  # ← ye add kar
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
